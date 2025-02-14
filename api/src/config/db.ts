d6cb81456861395ba93df80275931246aef3fe0b
Leigh <psychotropes661@gmail.com>
2025-02-14 16:18:57 +0100

    :construction_worker: (refactor) connexion database neon

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -5,9 +5,7 @@ dotenv.config();
 const { Pool } = require('pg')
 
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnautorized: false }
 });
+
